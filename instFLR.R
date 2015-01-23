3e84469df02957ab7c7ad8db1a42146800b3ceec
iagomosqueira <iago@mosqueira.eu>
2015-01-23 09:44:12 +0100

    New version of FLa4a 0.200.1

--- instFLR.R
+++ instFLR.R
@@ -6,11 +6,18 @@
 
 # NOTES:
 
+
 # GET pkg DESCRIPTIONs
 desc <- available.packages(contriburl = contrib.url("http://flr-project.org/R"))
 
 # OFFER FLR pkgs
 pkgs <- select.list(rownames(desc), multiple=TRUE, title="FLR Packages")
+
+# ---- FLash CHECK platform OS and arch
+arch <- .Platform
+
+if("FLash" %in% pkgs & arch$OS.type == "windows" & arch$r_arch != "i386")
+		stop("WARNING: FLash pkg requires 32 bit R in Windows!")
 
 # SUBSET desc
 desc <- desc[desc[, 'Package'] %in% pkgs,]
