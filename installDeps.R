3d4637640fe867aa92fe1b7907a9d0b931ee9f10
Iago Mosqueira <iago@mosqueira.eu>
2017-09-18 12:06:19 +0200

    Release of 2017.09.18

---
+++ installDeps.R
@@ -0,0 +1,12 @@
+# installDeps.R - DESC
+# /installDeps.R
+
+# Copyright European Union, 2017
+# Author: Iago Mosqueira (EC JRC) <iago.mosqueira@ec.europa.eu>
+#
+# Distributed under the terms of the European Union Public Licence (EUPL) V.1.1.
+
+# XX {{{
+# }}}
+
+install.packages(c("triangle", "copula", "coda", "mgcv", "ggplot2", "plyr", "reshape", "latticeExtra", "gridExtra"))
